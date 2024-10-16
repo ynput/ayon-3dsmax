2632609326cc8c6bee53f92b4dbdddaebc288777
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-10-16 10:51:44 +0200

    define app host name

--- package.py
+++ package.py
@@ -1,8 +1,10 @@
 name = "max"
 title = "Max"
 version = "0.2.4+dev"
+app_host_name = "max"
 client_dir = "ayon_max"
 
+ayon_server_version = ">=1.1.2"
 ayon_required_addons = {
     "core": ">0.3.2",
 }
