54f39cb5d643c935c5abdbc1456bdd37ca130f2c
Kayla Man <moonyuet2008@hotmail.com>
2024-11-20 16:35:03 +0800

    make sure the autobackup directory is created even without projection creation enabled

--- client/ayon_max/api/pipeline.py
+++ client/ayon_max/api/pipeline.py
@@ -218,7 +218,10 @@ def _set_project():
     os.makedirs(workdir, exist_ok=True)
     rt.pathConfig.setCurrentProjectFolder(workdir)
     enable_project_creation = project_settings["max"].get("enabled_project_creation")
-    if not enable_project_creation:
+    if enable_project_creation:
+        directory_count = rt.pathConfig.getProjectSubDirectoryCount()
+        autobackup_dir = rt.pathConfig.GetDir(rt.Name("autoback"))
+        os.makedirs(autobackup_dir, exist_ok=True)
         log.debug("Project creation disabled. Skipping project creation.")
         return
 
