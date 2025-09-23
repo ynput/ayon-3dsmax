3d9304c3115fdc114c9fe068141892dac6b5aa3d
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-09-23 16:59:37 +0200

    remove unused import

--- client/ayon_max/plugins/create/create_workfile.py
+++ client/ayon_max/plugins/create/create_workfile.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """Creator plugin for creating workfiles."""
-import ayon_api
-
 from ayon_core.pipeline import CreatedInstance, AutoCreator
 from ayon_max.api import plugin
 from ayon_max.api.lib import read, imprint
