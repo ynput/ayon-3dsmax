f2e7a84159462a521f838e40131a7365bee506d2
Kayla Man <moonyuet2008@hotmail.com>
2025-12-05 21:24:14 +0800

    restored the code of adding CreatedInstance as the product base type is already added

--- client/ayon_max/plugins/create/create_workfile.py
+++ client/ayon_max/plugins/create/create_workfile.py
@@ -64,7 +64,10 @@ class CreateWorkfile(plugin.MaxCreatorBase, AutoCreator):
             instance_node = self.create_node(product_name)
             data["instance_node"] = instance_node.name
             current_instance = CreatedInstance(
-                self.product_type, product_name, data, self
+            product_type=self.product_type,
+            product_name=product_name,
+            data=data,
+            creator=self
             )
             self._add_instance_to_context(current_instance)
             imprint(instance_node.name, current_instance.data)
