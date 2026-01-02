7ea75cb90871dd351be43cc32fde74d63cb49d06
Kayla Man <moonyuet2008@hotmail.com>
2026-01-02 23:40:54 +0800

    rename renderoutput for vray separate aov render

--- client/ayon_max/api/lib_rendersettings.py
+++ client/ayon_max/api/lib_rendersettings.py
@@ -134,7 +134,8 @@ class RenderSettings(object):
 
             if multipass_enabled:
                 rt.rendOutputFilename = output_filename
-                vr_settings.output_splitfilename = f"{output}.{img_fmt}"
+                aov_output = f"{output}_aov"
+                vr_settings.output_splitfilename = f"{aov_output}.{img_fmt}"
             else:
                 rt.rendOutputFilename = f"{output}_tmp..{img_fmt}"
             self.render_element_layer(output, width, height, img_fmt)
