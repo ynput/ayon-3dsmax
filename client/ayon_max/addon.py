3f5f14cc89f77039fc0519c5bd24adc9df558bfc
Kayla Man <64118225+moonyuet@users.noreply.github.com>
2026-01-13 13:29:56 +0800

    Update client/ayon_max/addon.py
    Co-authored-by: Roy Nieterau <roy_nieterau@hotmail.com>

--- client/ayon_max/addon.py
+++ client/ayon_max/addon.py
@@ -13,20 +13,25 @@ class MaxAddon(AYONAddon, IHostAddon):
     host_name = "max"
 
     def add_implementation_envs(self, env, _app):
-        # Add requirements to PYTHONPATH
-        new_python_paths = [
+        # Add requirements to ADSK_3DSMAX_STARTUPSCRIPTS_ADDON_DIR
+        new_addon_paths = [
             os.path.join(MAX_HOST_DIR, "startup")
         ]
-        old_python_path = env.get("ADSK_3DSMAX_STARTUPSCRIPTS_ADDON_DIR") or ""
-        for path in old_python_path.split(os.pathsep):
+        old_addon_paths = env.get("ADSK_3DSMAX_STARTUPSCRIPTS_ADDON_DIR") or ""
+        for path in old_addon_paths.split(os.pathsep):
             if not path:
                 continue
 
             norm_path = os.path.normpath(path)
-            if norm_path not in new_python_paths:
-                new_python_paths.append(norm_path)
+            if norm_path not in new_addon_paths:
+                new_addon_paths.append(norm_path)
 
-        env["ADSK_3DSMAX_STARTUPSCRIPTS_ADDON_DIR"] = new_python_paths
+        # 3dsmax docs state this is a semi-colon separated list. It does not
+        # state it uses the path separator, hence we use ; directly instead
+        # of os.pathsep
+        env["ADSK_3DSMAX_STARTUPSCRIPTS_ADDON_DIR"] = ";".join(
+            new_addon_paths
+        )
         # Remove auto screen scale factor for Qt
         # - let 3dsmax decide it's value
         env.pop("QT_AUTO_SCREEN_SCALE_FACTOR", None)
