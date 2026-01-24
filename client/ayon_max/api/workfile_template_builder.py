7fbd22914740cadfb395b7274ec5ead07035af99
Kayla Man <moonyuet2008@hotmail.com>
2026-01-24 17:53:25 +0800

    imprint the node not the node name

--- client/ayon_max/api/workfile_template_builder.py
+++ client/ayon_max/api/workfile_template_builder.py
@@ -152,7 +152,7 @@ class MaxPlaceholderPlugin(PlaceholderPlugin):
             if rt.getUserProp(target_node, key) is not None:
                 rt.deleteUserProp(target_node, key)
 
-        self.imprint(node_name, changed_values)
+        self.imprint(target_node, changed_values)
 
     def collect_placeholders(self):
         placeholders = []
