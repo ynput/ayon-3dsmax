b5aae07197a91f48e9d0dd33508a8dab4408ec48
Kayla Man <moonyuet2008@hotmail.com>
2026-02-03 18:46:29 +0800

    fix the is_hidden node not being correctly assigned

--- client/ayon_max/api/workfile_template_builder.py
+++ client/ayon_max/api/workfile_template_builder.py
@@ -192,7 +192,7 @@ class MaxPlaceholderPlugin(PlaceholderPlugin):
         if placeholder_set:
             placeholder_set = rt.Container(name=PLACEHOLDER_SET)
         placeholder_set.children = node
-        node.isHidden = True
+        placeholder_set.isHidden = True
 
     def delete_placeholder(self, placeholder):
         """Remove placeholder if building was successful
