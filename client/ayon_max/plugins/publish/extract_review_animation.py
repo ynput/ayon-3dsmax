8f4944056015a91db8671705b5a87d4c67970ba7
Kayla Man <moonyuet2008@hotmail.com>
2026-02-06 23:11:45 +0800

    check on render productBaseType and return for review families

--- client/ayon_max/plugins/publish/extract_review_animation.py
+++ client/ayon_max/plugins/publish/extract_review_animation.py
@@ -18,7 +18,7 @@ class ExtractReviewAnimation(publish.Extractor):
     families = ["review"]
 
     def process(self, instance):
-        if instance.data.get("productType") == "maxrender":
+        if instance.data.get("productBaseType") == "render":
             self.log.debug(
                 f"The instance {instance.name} is Render product type, "
                 "skipping review animation extraction.")
