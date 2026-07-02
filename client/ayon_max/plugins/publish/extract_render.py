0f38a37ae995384e34963b76c3b8623bf6c52ce0
Kayla Man <moonyuet2008@hotmail.com>
2026-07-02 20:44:19 +0800

    revert vfb changes

--- client/ayon_max/plugins/publish/extract_render.py
+++ client/ayon_max/plugins/publish/extract_render.py
@@ -40,6 +40,7 @@ class ExtractLocalRender(publish.Extractor):
             for frame in instance.data["expectedFrameRange"]:
                 _, cancelled = rt.render(
                     frame=frame,
+                    vfb=False,
                     camera=camera_node,
                     cancelled=pymxs.byref(None)
                 )
