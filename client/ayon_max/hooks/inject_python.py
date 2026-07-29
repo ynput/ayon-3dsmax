c615c76f4faf6e59be5b7fb6b4783bea50a0685c
Kayla Man <moonyuet2008@hotmail.com>
2026-07-29 14:08:57 +0800

    clean up inject_python.py

--- client/ayon_max/hooks/inject_python.py
+++ client/ayon_max/hooks/inject_python.py
@@ -2,13 +2,12 @@
 """Pre-launch hook to inject python environment."""
 import os
 from ayon_applications import PreLaunchHook, LaunchTypes
-from ayon_max import MAX_HOST_DIR
 
 
 class InjectPythonPath(PreLaunchHook):
     """Inject AYON environment to 3dsmax.
 
-    Note that this works in combination with 3dsmax startup script that
+    Note that this works in combination whit 3dsmax startup script that
     is translating it back to PYTHONPATH for cases when 3dsmax drops PYTHONPATH
     environment.
 
@@ -18,13 +17,4 @@ class InjectPythonPath(PreLaunchHook):
     launch_types = {LaunchTypes.local}
 
     def execute(self):
-        pythonpath_value = self.launch_context.env.get("PYTHONPATH")
-        if pythonpath_value is None:
-            pythonpath_value = os.environ.get("PYTHONPATH", "")
-
-        paths = [path for path in pythonpath_value.split(os.pathsep) if path]
-        addon_client_path = os.path.join(MAX_HOST_DIR, "client")
-        if addon_client_path not in paths:
-            paths.append(addon_client_path)
-
-        self.launch_context.env["MAX_PYTHONPATH"] = os.pathsep.join(paths)
+        self.launch_context.env["MAX_PYTHONPATH"] = os.environ["PYTHONPATH"]
