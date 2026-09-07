cc9af7c2129827b81ac9f855737e086bb06537be
Kayla Man <moonyuet2008@hotmail.com>
2026-09-07 16:47:34 +0800

    pop the families data before the imprint data

--- client/ayon_max/api/plugin.py
+++ client/ayon_max/api/plugin.py
@@ -504,6 +504,7 @@ class MaxCacheCreator(Creator, MaxTyFlowDataCreatorBase):
                 instance_node = new_product_name
                 created_inst["instance_node"] = instance_node
                 node.name = instance_node
+                created_inst.pop("families", None)
 
             imprint(
                 instance_node,
