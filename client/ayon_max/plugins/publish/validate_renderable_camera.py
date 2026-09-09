9772adfc0f0fdf75da4f049ba3c6181be29e9ca6
Kayla Man <moonyuet2008@hotmail.com>
2026-09-09 15:00:04 +0800

    add two invalid check on camera nodes and mismatch camera

--- client/ayon_max/plugins/publish/validate_renderable_camera.py
+++ client/ayon_max/plugins/publish/validate_renderable_camera.py
@@ -26,21 +26,54 @@ class ValidateRenderableCamera(pyblish.api.InstancePlugin,
     def process(self, instance):
         if not self.is_active(instance.data):
             return
-        if not instance.data["cameras"]:
+        if self.get_invalid_cmaera_nodes(instance):
             raise PublishValidationError(
                 "No renderable Camera found in scene."
             )
+        if self.get_mismatch_camera(instance):
+            raise PublishValidationError(
+                "The current viewport camera does not match "
+                "the target renderable camera."
+            )
+
+    @ classmethod
+    def get_invalid_cmaera_nodes(cls, instance):
+        if not instance.data["cameras"]:
+           return True
+        return False
+
+    @classmethod
+    def get_mismatch_camera(cls, instance):
+        if instance.data["multiCamera"]:
+            cls.log.warning("Multiple cameras detected in the scene. Skipping validation.")
+            return None
+        camera = next(iter(instance.data.get("cameras", [])), None)
+        if rt.viewport.GetCamera() != rt.getNodeByName(camera):
+            return camera
+        return None
 
     @classmethod
     def repair(cls, instance):
+        invalid_camera_nodes = cls.get_invalid_cmaera_nodes(instance)
+        if invalid_camera_nodes:
+            rt.viewport.setType(rt.Name("view_camera"))
+            camera = rt.viewport.GetCamera()
+            cls.log.info(f"Camera {camera} set as renderable camera")
+            cls._set_arnold_camera(camera)
+            instance.data["cameras"] = [camera.name]
+            return
+        mismatch_camera = cls.get_mismatch_camera(instance)
+        if mismatch_camera:
+            rt.viewport.setCamera(rt.getNodeByName(mismatch_camera))
+            cls.log.info(f"Camera {mismatch_camera} set as renderable camera")
+            cls._set_arnold_camera(rt.getNodeByName(mismatch_camera))
+            instance.data["cameras"] = [mismatch_camera]
 
-        rt.viewport.setType(rt.Name("view_camera"))
-        camera = rt.viewport.GetCamera()
-        cls.log.info(f"Camera {camera} set as renderable camera")
+    @classmethod
+    def _set_arnold_camera(cls, camera):
         renderer_class = get_current_renderer()
         renderer = str(renderer_class).split(":")[0]
         if renderer == "Arnold":
             arv = rt.MAXToAOps.ArnoldRenderView()
             arv.setOption("Camera", str(camera))
             arv.close()
-        instance.data["cameras"] = [camera.name]
